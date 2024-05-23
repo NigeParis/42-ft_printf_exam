e8977c28bbd7ee93c386003ea99b2172a16fad75
Nigel Robinson <nrobinso@paul-f5Ar1s2.clusters.42paris.fr>
2024-05-23 15:04:16 +0200

    bug hex fixed xast unsigned int

--- version5/ft_printf.c
+++ version5/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 12:40:32 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/05/23 13:33:18 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/05/23 15:02:56 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,21 @@ int ft_putstr(char *str)
 	return (nb_print);
 }
 
-int	ft_putnbr(long int nbr, int base)
+int	ft_putnbr(long long int nbr, int base)
 {
 	const char basestr[16] = "0123456789abcdef";
+	//static int nb_print = 0;
 	static int nb_print;
 	int sign = 0; 
+	int ret;
 
 	nb_print = 0;
 	if (base < 2)
 		return (0);
 	if (base == 10)
 		nbr = (int)nbr;
+	if (base == 16)
+		nbr = (unsigned int)nbr;
 	if(nbr < 0)
 	{
 		if (base == 10)
@@ -59,12 +63,13 @@ int	ft_putnbr(long int nbr, int base)
 		}
 		nbr = nbr * -1;	
 	}
-	if(nbr > base -1)
+	if(nbr > (base -1))
 	{
 		ft_putnbr((nbr / base), base);
 	}
 	nb_print += ft_putchar((basestr[nbr % base]));
-	return (nb_print + sign);
+	ret = nb_print + sign;
+	return (ret);
 }
 
 
@@ -87,7 +92,7 @@ int	ft_printf(const char *str, ...)
 			if (str[i] && str[i] == 's')
 				nb_print += ft_putstr(va_arg(input, char *));
 			if ((str[i] && str[i] == 'd') || (str[i] && str[i] == 'i'))
-				nb_print += ft_putnbr(va_arg(input, long int), 10);
+				nb_print += ft_putnbr(va_arg(input, int), 10);
 		    if (str[i] && str[i] == 'x')
 				nb_print += ft_putnbr(va_arg(input, long int), 16);
 			if (str[i] && str[i] == '%')
@@ -106,17 +111,25 @@ int	ft_printf(const char *str, ...)
 int main(void)
 {
 	int nb;
-	char str[] = "hello";
-	int nbr = 1;
+	//char str[] = "hello";
+	int nbr = 42;
 	int nbr2 = -1;
+	int nbr4 = -2;
 
 	nb = 0;
 
-	nb = ft_printf("%s %d %x%%",str, nbr, nbr2);
+	nb = ft_printf("%d %x %x %d %x %x %x %x %x %%", nbr2, nbr, nbr2, nbr, nbr, nbr2, nbr2, nbr, nbr4);
 	printf("\nnb printed :%d\n", nb);
 
-	   nb = printf("%s %d %x%%",str, nbr, nbr2);
+	   nb = printf("%d %x %x %d %x %x %x %x %x %%", nbr2, nbr, nbr2, nbr, nbr, nbr2, nbr2, nbr, nbr4);
 	printf("\nnb printed :%d\n", nb);
+	
+	nb = ft_printf("%x %x %x %d %x%%", nbr2, nbr2, nbr, nbr, nbr4);
+	printf("\nnb printed :%d\n", nb);
+
+	   nb = printf("%x %x %x %d %x%%", nbr2, nbr2, nbr, nbr, nbr4);
+	printf("\nnb printed :%d\n", nb);
+
 	
 	nb = ft_printf(NULL);
 	printf("\nnb printed :%d\n", nb);
